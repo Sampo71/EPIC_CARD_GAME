7667874d91365f41b53b26dd4f045f0f2df3cecb
SnaksiXD <aleksin.roskat@gmail.com>
2023-09-06 19:28:23 +0300

    beta card

---
+++ Sampo/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+import Card from './components/Card'
+
+export default function App(){
+  return(
+    <>
+    <h1>Hello world!</h1>
+    <Card/>
+    </>
+  );
+}
---
+++ Sampo/src/components/Card.jsx
@@ -0,0 +1,23 @@
+
+export default function Card(){
+    return(
+        <div>
+            <img src = "http://placekitten.com/120/100"/>
+            <ul>
+                <li>
+                <span>scratch </span>
+                <span>9</span>
+                </li>
+                <li>
+                    <span>cuteness </span>
+                    <span>1</span>
+                </li>
+                <li>
+                    <span>weight </span>
+                    <span>2</span>
+                </li>
+            </ul>
+       
+        </div>
+    );
+}
