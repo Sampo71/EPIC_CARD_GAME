3b6c6f1dad873d9e8313ead86748efd8d73da363
SnaksiXD <aleksin.roskat@gmail.com>
2023-09-20 19:28:38 +0300

    better cards and comparason

--- Sampo/src/App.jsx
+++ Sampo/src/App.jsx
@@ -3,14 +3,47 @@ import Card from './components/Card'
 
 const playerCard = {
   image: 'http://placekitten.com/140/120',
-  stats: [{name:'scratching', value: 9}]
+  stats: [{name:'scratching', value: 9},
+          {name:'tackling', value: 5},
+          {name:'running', value: 8}]
 };
 
+const opponentCard = {
+  image: 'http://placekitten.com/140/120?image=2', 
+  stats: [{name: 'scratching', value: 8},
+          {name: 'tackling', value: 4},
+          {name:'running', value: 4}]
+}
+
 export default function App(){
+
+  function compareCards(){
+    const playerStat = playerCard.stats[0];
+    const opponentStat = opponentCard.stats[0];
+
+    let result = '';
+
+    if(playerStat.value > opponentStat.value){
+      result = 'winner';
+    }
+    
+    if(playerStat.value < opponentStat.value){
+      result = 'loss';
+    }
+
+    if(playerStat.value == opponentStat.value){
+      result = 'draw';
+    }
+
+    console.log(result)
+  }
+
   return(
     <>
     <h1>Hello world!</h1>
     <Card card = {playerCard}/>
+    <button onClick={compareCards} type="button">play</button>
+    <Card card = {opponentCard}/>
     </>
   );
 }
--- Sampo/src/components/Card.jsx
+++ Sampo/src/components/Card.jsx
@@ -4,18 +4,13 @@ export default function Card({card}){
         <div className="card">
             <img src = {card.image}/>
             <ul className="stat-list">
-                <li className="stat-list-item">
-                <span>{card.stats[0].name} </span>
-                <span>{card.stats[0].value}</span>
+                {card.stats.map((stat,index) =>(
+                <li className="stat-list-item" key={index}>
+                <span>{stat.name} </span>
+                <span>{stat.value}</span>
                 </li>
-                <li>
-                    <span>cuteness </span>
-                    <span>15</span>
-                </li>
-                <li>
-                    <span>weight </span>
-                    <span>20</span>
-                </li>
+
+                ))}
             </ul>
        
         </div>
