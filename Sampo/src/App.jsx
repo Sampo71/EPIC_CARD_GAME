6396059d8cb41b4d449cba98cc1dd45afe27f477
SnaksiXD <aleksin.roskat@gmail.com>
2023-09-27 19:30:10 +0300

    randomized cards

--- Sampo/src/App.jsx
+++ Sampo/src/App.jsx
@@ -1,49 +1,68 @@
 import './App.css'
 import Card from './components/Card'
+import { useState } from 'react';
+
+const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min +1) + min);
 
 const playerCard = {
-  image: 'http://placekitten.com/140/120',
-  stats: [{name:'scratching', value: 9},
-          {name:'tackling', value: 5},
-          {name:'running', value: 8}]
+  image: 'http://placekitten.com/140/120?image=' + getRandomInt(0,15),
+  stats: [{name:'scratching', value: getRandomInt(1,1000)},
+          {name:'tackling', value: getRandomInt(1,1000)},
+          {name:'running', value: getRandomInt(1,1000)}],
 };
 
 const opponentCard = {
-  image: 'http://placekitten.com/140/120?image=2', 
-  stats: [{name: 'scratching', value: 8},
-          {name: 'tackling', value: 4},
-          {name:'running', value: 4}]
+  image: 'http://placekitten.com/140/120?image=' + getRandomInt(0,15), 
+  stats: [{name: 'scratching', value: getRandomInt(1,1000)},
+          {name: 'tackling', value: getRandomInt(1,1000)},
+          {name:'running', value: getRandomInt(1,1000)}]
 }
 
+const createCard = (index) =>({
+  image: 'http://placekitten.com/140/120?image=' + index,
+  stats: [{name:'scratching', value: getRandomInt(1,1000)},
+          {name:'tackling', value: getRandomInt(1,1000)},
+          {name:'running', value: getRandomInt(1,1000)}],
+  id: crypto.randomUUID()
+});
+
+const deck = Array(16).fill(null).map((_,index)=>createCard(index))
+
 export default function App(){
+
+  const [result, setResult] = useState('');
 
   function compareCards(){
     const playerStat = playerCard.stats[0];
     const opponentStat = opponentCard.stats[0];
 
-    let result = '';
-
     if(playerStat.value > opponentStat.value){
-      result = 'winner';
+      setResult('Winner');
     }
     
     if(playerStat.value < opponentStat.value){
-      result = 'loss';
+      setResult('Loss');
     }
-
+    
     if(playerStat.value == opponentStat.value){
-      result = 'draw';
+      setResult('Draw');
     }
-
-    console.log(result)
   }
 
   return(
     <>
-    <h1>Hello world!</h1>
-    <Card card = {playerCard}/>
-    <button onClick={compareCards} type="button">play</button>
-    <Card card = {opponentCard}/>
+    <h1>EPIC_CARD_GAME!</h1>
+    <div className='Game'>
+      <Card card = {playerCard}/>
+
+      <div className='center-area'>
+        <p>{result || ''}</p>
+        <button onClick={compareCards} type="button">play</button>
+      </div>
+
+      <Card card = {opponentCard}/>
+    </div>
+    
     </>
   );
 }
