730d3d5e1193887df31b46a86dd97730c49383a7
SnaksiXD <aleksin.roskat@gmail.com>
2023-12-13 19:30:19 +0200

    something i guess

---
+++ Sampo/src/components/breakout/BallMovement.jsx
@@ -0,0 +1,25 @@
+export function BallMovement(ctx, ballObj){
+    let data = new Ball(ballObj.x, ballObj.y, ballObj.rad);
+    data.Draw(ctx);
+    ballObj.x += ballObj.dx;
+    ballObj.y += ballObj.dy
+
+}
+
+class Ball{
+    constructor(x ,y, rad){
+        this.x = x;
+        this.y = y;
+        this.rad = rad;
+    }
+
+    Draw(ctx){
+        ctx.beginPath();
+        ctx.fillStyle = "red";
+        ctx.arc(this.x, 75, 50, 0, Math.PI * 2);
+        ctx.strokeStyle = "black";
+        ctx.lineWidth = 3;
+        ctx.fill();
+        ctx.stroke();
+    }
+}
--- Sampo/src/components/breakout/Breakout.jsx
+++ Sampo/src/components/breakout/Breakout.jsx
@@ -1,7 +1,25 @@
-import react from "react";
+import React, {useEffect, useRef} from "react";
+import { BallMovement } from "./BallMovement";
+import data from "./data";
 
+let {ballObj} = data;
 export default function Breakout(){
+    const canvasRef = useRef(null);
+
+    useEffect(()=>{
+        // ctx.fillStyle = "green";
+        // ctx.fillRect(10, 10, 100, 150);
+        const render = ()=>{
+        const canvas = canvasRef.current;
+        const ctx = canvas.getContext('2d');
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        BallMovement(ctx, ballObj);
+        requestAnimationFrame(render);
+       }
+       render();
+    }, [])
+
     return(
-        <canvas id="canvas" height="500px" width="800px"/>
+        <canvas id="canvas" ref={canvasRef} height="500px" width="800px"/>
     )
 }
